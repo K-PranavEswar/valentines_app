30a3eb2184d05cfc6ec48f3d9b11b056c1d914cf
Pranav Eswar <pranav1495.pranavartist1@gmail.com>
2026-01-25 16:36:00 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://valentinesapp26.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
   })
